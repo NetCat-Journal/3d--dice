4be3c21291adf4466b62e2248a0b58fd129683db
NetCat-Journal <magdab.78b@gmail.com>
2025-11-02 16:33:02 -0500

    dots on dice

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,102 @@ export default function Home() {
           />
           <meshPhongMaterial color="#f3f3f3" />
         </mesh>
+        {/* one dot front*/}
+        <mesh position={[0, 0, 0.51]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
+        {/* six dots back */}
+        <mesh position={[0.25, 0.25, -0.51]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[-0.25, 0.25, -0.51]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[0.25, -0.25, -0.51]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[-0.25, -0.25, -0.51]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[0.25, 0, -0.51]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[-0.25, 0, -0.51]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
+        {/* four dots bottom */}
+        <mesh position={[-0.25, -0.51, 0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[0.25, -0.51, 0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[-0.25, -0.51, -0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[0.25, -0.51, -0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
+        {/*two dots top*/}
+        <mesh position={[0.25, 0.51, -0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[-0.25, 0.51, 0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
+        {/* three dots left */}
+        <mesh position={[-0.51, 0.25, 0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[-0.51, 0, 0]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[-0.51, -0.25, -0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
+        {/* five dots right */}
+        <mesh position={[0.51, 0.25, 0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[0.51, -0.25, 0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[0.51, 0.25, -0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[0.51, -0.25, -0.25]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        <mesh position={[0.51, 0, 0]}>
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
         <mesh position-y={-1.0} rotation-x={-Math.PI / 2} receiveShadow>
           <planeGeometry args={[5, 5]} />
           <meshStandardMaterial color="white" roughness={0.8} metalness={0.2} />
